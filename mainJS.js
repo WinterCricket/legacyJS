e2c7da99d48117bda118fcdd99d0d3705136c9ad
WinterCricket <hodhodhod2003@gmail.com>
2018-08-21 19:56:20 -0500

    rename files and wrap methods in a function

---
+++ mainJS.js
@@ -0,0 +1,11 @@
+
+function buttControl() {
+var rightButton = document.getElementById('rightbutton'); 
+ alert(rightButton);
+ rightButton.value = "WTF?";
+
+}
+document.getElementById('leftbutton').onclick=function(){
+	alert(this.value);
+}
+
